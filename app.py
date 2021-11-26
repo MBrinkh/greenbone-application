d03b7551b2b56a96cc7ccb1224544f3e286247ed
MBrinkh <87913239+MBrinkh@users.noreply.github.com>
2021-11-26 04:45:22 +0100

    Add mock employees to app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from api import api_Blueprint
+from api_client import Api_Client
 
 
 def create_app():
@@ -13,7 +14,23 @@ def create_app():
 app = create_app()
 
 
+# In a real world scenario this client would be injected via DI with the appropriate configuration
+# in this small scale demo app we will stick with a local initialization
+client = Api_Client('http://localhost:5000/api/computer')
+
+#Should come from a rest api, for simplicity its just hardcoded into the app
+db_employees = [
+    { 'abbreviation': 'ABC' },
+    { 'abbreviation': 'DEF' },
+    { 'abbreviation': 'GBR' },
+]
+
+
 @app.route('/')
 def index():
 
-    return render_template('index.html')
+    response = client.get()
+
+    computers = response['data'] if response['success'] else []
+
+    return render_template('index.html', employees = db_employees, computers = computers)
